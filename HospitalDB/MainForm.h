5561fa10806144855bf2bb22493483cdc4a37a64
theParas <parassth63@gmail.com>
2022-07-02 14:49:30 +0545

    basic setup

---
+++ HospitalDB/MainForm.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace HospitalDB {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Source
+	/// </summary>
+	public ref class HospitalDB : public System::Windows::Forms::Form
+	{
+	public:
+		HospitalDB(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~HospitalDB()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// HospitalDB
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSizeMode = System::Windows::Forms::AutoSizeMode::GrowAndShrink;
+			this->BackColor = System::Drawing::Color::LightGray;
+			this->ClientSize = System::Drawing::Size(1262, 673);
+			this->MaximizeBox = false;
+			this->Name = L"HospitalDB";
+			this->Text = L"HospitalDB";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
